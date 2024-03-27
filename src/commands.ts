ad88c995e6f1513cf1ffe5d889cb63b36277e03d
risingsun007 <oab102@hotmail.com>
2024-03-27 10:41:26 -0400

    debug2

--- src/commands.ts
+++ src/commands.ts
@@ -23,6 +23,45 @@ const getFaceQuizCommand =
     try {
       await ack();
       const slackUsers = await fetchUsers({ app });
+      console.log("slackUsers: ", slackUsers);
+      console.log("command.user_id: ", command.user_id);
+
+      const quiz = await getFaceQuiz({
+        exclude: [command.user_id],
+        slackUsers,
+      });
+      console.log("quiz: ", quiz.blocks);
+      await app.dm({ user: command.user_id, blocks: quiz.blocks });
+    } catch (error) {
+      if (error instanceof MessageError) {
+        await app.dm({
+          user: command.user_id,
+          text: (error as MessageError).message,
+        });
+      } else {
+        throw error;
+      }
+    }
+  };
+
+const getExplorerScan =
+  (app: ChatBot) =>
+  async ({
+    command,
+    ack,
+    say,
+  }: {
+    command: SlashCommand;
+    ack: AckFn<string | RespondArguments>;
+    say: SayFn;
+  }) => {
+    console.log("command: ", command);
+    console.log("ack: ", ack);
+    console.log("says: ", say);
+
+    try {
+      await ack();
+      const slackUsers = await fetchUsers({ app });
       const quiz = await getFaceQuiz({
         exclude: [command.user_id],
         slackUsers,
@@ -42,4 +81,5 @@ const getFaceQuizCommand =
 
 export const addSlashCommands = (app: ChatBot) => {
   app.command("/facequiz", getFaceQuizCommand(app));
+  app.command("/explorer-scan", getExplorerScan(app));
 };
