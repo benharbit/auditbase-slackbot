0f65d8c24bb433e3da748b369e914d7f35c81e8e
risingsun007 <oab102@hotmail.com>
2024-03-28 17:43:48 -0400

    Add file upload functionality to placeUpload.ts

--- src/auditbase/placeUpload.ts
+++ src/auditbase/placeUpload.ts
@@ -1,12 +1,18 @@
 import { AUDITBASE_API_SERVER } from "../constants";
 import { WebClient } from "@slack/web-api";
 import axios from "axios";
+import fs from "fs";
 
 const client = new WebClient(process.env.SLACK_BOT_TOKEN);
 
 async function getFileList() {
   try {
     // Call the files.list API method
+    const uploadResult = await client.files.upload({
+      channels: "#auditbase-dev",
+      file: fs.createReadStream("./README.md"),
+    });
+    console.log("uploadResult", JSON.stringify(uploadResult));
     const result = await client.files.list();
     console.log("result", JSON.stringify(result));
     if (result?.files) {
