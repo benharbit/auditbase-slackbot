c2462d23b43eb96649fbd179668f08b474b262d1
Omar <oab102@hotmail.com>
2024-05-28 11:19:04 -0400

    Refactor convertToJson function and add debug print statement

--- src/http.ts
+++ src/http.ts
@@ -83,6 +83,8 @@ const convertToMarkedDown = (ai_results: string) => {
     return ai_results;
   }
 
+  print("json_object: ", JSON.stringify(json_object));
+
   const markdown1 = convertToJson(json_object);
   return markdown1;
 };
